5134e240994ed49e28c0aa8283eff44a6b21b431
SumaSubramani <34449679+SumaSubramani@users.noreply.github.com>
2022-04-04 13:39:56 +0200

    Update test_01.py

--- testcases/test_01.py
+++ testcases/test_01.py
@@ -7,9 +7,7 @@ class TextSearch(BaseTest):
         super().setUp()
         self.base_selenium.get_url(url=self.url)
         
-    def test006_inurl_feature(self, word):
+    def test_feature(self, word):
         self.search_page.inurl_search(text=word)
-
-        self.base_selenium.LOGGER.info('Verify that all results url has {}'.format(word))
         for result in self.results_page.get_results_urls():
             self.assertIn(word.lower(), result.lower())
